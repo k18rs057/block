837099b0909a72a1e35f1da732f63eb8d071d67b
k18rs057 <k18rs057@st.kyusan-u.ac.jp>
2019-11-25 14:03:26 +0900

    ncmbControllerへの追加

--- www/keys.js
+++ www/keys.js
@@ -1,2 +1,3 @@
 // This is a JavaScript file
-
+var appKey = "20f175a341b0f00a62995f8f29ad6ace6e4beb8e3d65df206ba0ff2ee6fcd923";
+var clientKey = "9a657499f01ed68abe0b775fd53d4480561b2aa0208c3442892441fdcafa3202";
